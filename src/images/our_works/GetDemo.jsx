8be9330da289b9459a4bd1e0647f973234f74965
MdodoIng <mdodoyt2@gmail.com>
2023-01-06 10:12:21 +0530

    modified:   package-lock.json 	modified:   src/images/our_works/GetDemo.jsx 	modified:   src/pages/404.js

--- src/images/our_works/GetDemo.jsx
+++ src/images/our_works/GetDemo.jsx
@@ -2,16 +2,18 @@ import React from "react";
 import { Button } from "../../ui/Button";
 
 const GetDemo = () => {
-  return <div className="py-[70px] px-[122px] flex w-full my-20">
-    <div>
-      <h1 className="font-medium text-6xl text-white opacity-60">Ready to try <span className="opacity-100">Grafixy?</span></h1>
-      <p></p>
-      <Button />
-      <Button />
+  return (
+    <div className="py-[70px] px-[122px] flex w-full my-20">
+      <div>
+        <h1 className="font-medium text-6xl text-white opacity-60">
+          Ready to try <span className="opacity-100">Grafixy?</span>
+        </h1>
+        <p></p>
+        <Button />
+        <Button />
+      </div>
     </div>
-
-    
-  </div>;
+  );
 };
 
 export default GetDemo;
