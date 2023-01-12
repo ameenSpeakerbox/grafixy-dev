ffcfdb41c0ba669c301590a5e831f0f8102a326f
MdodoIng <mdodoyt2@gmail.com>
2023-01-12 13:42:44 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/components/home/HeroSectionFeatures.jsx

--- src/components/home/HeroSectionFeatures.jsx
+++ src/components/home/HeroSectionFeatures.jsx
@@ -1,3 +1,5 @@
+import TawkMessengerReactUmd from "@tawk.to/tawk-messenger-react";
+import TawkMessengerReact from "@tawk.to/tawk-messenger-react";
 import { StaticImage } from "gatsby-plugin-image";
 import React from "react";
 import { heroSection_features } from "../../lib/dummyData";
@@ -32,14 +34,14 @@ const HeroSectionFeatures = () => (
           </p>
         </div>
       ))}
-    <div className="bg-[#4600B2] 2xl:py-[51px]  2xl:px-[76px] sm:py-5 sm:px-10 px-8 py-10 border-t border-l border-black 2xl:pb-[35px] pb-[20px] relative grid sm:h-[110%] w-full h-full">
-      <h2 className="font-bold 2xl:text-2xl lg:text-xl sm:text-sm text-[22px] 2xl:leading-[110%] leading-5 text-white  flex shrink-0 w-max pl-10 relative">
-        <MessageIcon className="absolute lg:-left-4 -left-[2px] 2xl:w-[48px] 2xl:h-[48px] sm:w-[28px] sm:h-[28px] w-[40px] h-[40px]" />
-        NEED TO KNOW MORE? <br />
-        LET’S HAVE A CHAT
-      </h2>
-      <div className="flex justify-end">
-        <StaticImage
+      <div className="bg-[#4600B2] 2xl:py-[51px]  2xl:px-[76px] sm:py-5 sm:px-10 px-8 py-10 border-t border-l border-black 2xl:pb-[35px] pb-[20px] relative grid sm:h-[110%] w-full h-full">
+        <h2 className="font-bold 2xl:text-2xl lg:text-xl sm:text-sm text-[22px] 2xl:leading-[110%] leading-5 text-white  flex shrink-0 w-max pl-10 relative">
+          <MessageIcon className="absolute lg:-left-4 -left-[2px] 2xl:w-[48px] 2xl:h-[48px] sm:w-[28px] sm:h-[28px] w-[40px] h-[40px]" />
+          NEED TO KNOW MORE? <br />
+          LET’S HAVE A CHAT
+        </h2>
+        <div className="flex justify-end">
+          {/* <StaticImage
           src="../../images/home/AJID.png"
           layout="fullWidth"
           width={135}
@@ -47,11 +49,13 @@ const HeroSectionFeatures = () => (
           objectFit="cover"
           alt="ajid"
           className="lg:w-[135px] lg:h-[135px] sm:w-[80px] sm:h-[80px] h-[135px] w-[135px]"
-        />
+        /> */}
+
+          <TawkMessengerReact propertyId="63bfbe8cc2f1ac1e202d05f5"
+                widgetId="default" />
+        </div>
       </div>
     </div>
-    </div>
-
   </div>
 );
 
