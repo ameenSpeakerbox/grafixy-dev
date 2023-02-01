fddba0a7b9e97e46d7c01bf1ef4cb4d538df9684
MdodoIng <mdodoyt2@gmail.com>
2023-02-01 10:57:32 +0530

    modified:   src/components/why_grafixy/HeroSection.jsx 	modified:   src/components/why_grafixy/WhyGetGrafixy .jsx 	modified:   src/components/why_grafixy/scope_of_service/HeroSection.jsx 	modified:   src/style/global.css

--- src/components/why_grafixy/WhyGetGrafixy .jsx
+++ src/components/why_grafixy/WhyGetGrafixy .jsx
@@ -9,26 +9,26 @@ const WhyGetGrafixy = () => {
         <h1 className="font-semibold 2xl:text-[45px] sm:text-4xl text-3xl tracking-[-2%] text-[#210153]">
           Why get grafixy?
         </h1>
-        <p className="text-[#6E3CBC] font-nunito 2xl:text-2xl lg:text-xl text-lg max-w-[837px] leading-[100%]">
+        <p className="text-[#6E3CBC] font-nunito 2xl:text-2xl lg:text-xl text-lg max-w-[837px] leading-[120%]">
           Maybe its time to stop hiring designers that are unreliable and
           expensive. Get your own design team at just a fraction of the cost.
         </p>
       </div>
       <div className="grid md:grid-cols-2 2xl:mt-20 lg:mt-16 mt-10 2xl:gap-16 lg:gap-14 gap-8 place-items-start md:w-max w-full">
         {getGrafixy.map((item) => (
-          <div key={item.id} className="flex items-center justify-start 2xl:gap-6 lg:gap-5 gap-4">
-            <div className="grid place-content-center w-24 h-24">
+          <div key={item.id} className="grid grid-flow-col items-center justify-start 2xl:gap-6 lg:gap-5 gap-2">
+            <div className="flex items-center justify-center w-24 h-24">
               <MainImage
                 src={item.icon}
                 alt="icon"
                 width={90}
                 height={90}
-                className="object-contain w-full h-full 2xl:max-h-20 max-h-16"
+                className={`object-contain w-max h-full 2xl:max-h-20 max-h-16 ${item.id === 2 && "mr-8"}`}
               />
             </div>
             <div className="grid place-content-start gap-1">
               <h3 className="font-medium 2xl:text-[28px] lg:text-2xl text-xl tracking-[-2%] text-[#28075A] ">{item.title}</h3>
-              <p className="font-nunito 2xl:text-lg lg:text-base text-sm leading-[100%] font-normal text-[#6E3CBC] max-w-[310px]">{item.subtitle}</p>
+              <p className="font-nunito 2xl:text-lg lg:text-base text-sm leading-[120%] font-normal text-[#6E3CBC] max-w-[310px]">{item.subtitle}</p>
             </div>
           </div>
         ))}
--- src/components/why_grafixy/scope_of_service/HeroSection.jsx
+++ src/components/why_grafixy/scope_of_service/HeroSection.jsx
@@ -4,9 +4,11 @@ import heroPic from "../../../images/why_grafixy/scope of service hero picture.w
 
 const HeroSection = () => {
   return (
-    <div className=" bg-[#6E3CBC] scope_of_service--heroSection flex-col w-full relative  overflow-hidden justify-end z-0 flex items-center  2xl:px-[147px] 
+    <div
+      className=" bg-[#6E3CBC] scope_of_service--heroSection flex-col w-full relative  overflow-hidden justify-end z-0 flex items-center  2xl:px-[147px] 
     sm:px-[80px] 
-    px-8  ">
+    px-8  "
+    >
       <MainImage
         src={heroPic}
         alt="heroPic"
@@ -16,10 +18,10 @@ const HeroSection = () => {
       />
       <span className="bg-[#1A0143] absolute inset-0 bg-opacity-95 -z-20" />
       <p className="leading-[134%] 2xl:text-[40px] text-3xl font-semibold  text-white max-w-[867px] text-center">
-      Scope of service
+        Scope of service
       </p>
-      <div className="inline-flex text-white font-semibold 2xl:text-[28px] text-2xl leading-[134%] tracking-[3px] gap-[47px] mt-[14px] mb-14 text-center">
-        <p>Over 150+ design services covered</p>
+      <div className="text-white font-semibold 2xl:text-[25px] text-2xl leading-[134%] gap-[47px] mt-1 mb-10 text-center font-nunito tracking-[-2%]">
+        Over 150+ design services covered
       </div>
     </div>
   );
