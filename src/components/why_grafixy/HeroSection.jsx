8f51b0dc5bb47cb95e4267cdd23b6b1ab1d23474
MdodoIng <mdodoyt2@gmail.com>
2023-02-01 09:50:22 +0530

    modified:   src/components/why_grafixy/HeroSection.jsx 	modified:   src/components/why_grafixy/WhyGetGrafixy .jsx

--- src/components/why_grafixy/HeroSection.jsx
+++ src/components/why_grafixy/HeroSection.jsx
@@ -6,20 +6,20 @@ const HeroSection = () => {
   return (
     <div className=" bg-[#6E3CBC] why_grafixy--heroSection flex-col w-full relative  overflow-hidden justify-end z-0 flex items-center  2xl:px-[147px] 
     sm:px-[80px] 
-    px-8  ">
+    px-8 sm:py-14 py-6">
       <MainImage
         src={heroPic}
         alt="heroPic"
         width={100}
         height={100}
-        className="object-cover origin-center w-full h-full absolute -z-10 opacity-[25%]"
+        className="object-cover origin-center w-full h-full absolute -z-10 opacity-[25%] top-0 left-0"
       />
       <span className="bg-[#1A0143] absolute inset-0 bg-opacity-95 -z-20" />
-      <p className="leading-[134%] 2xl:text-[40px] text-3xl font-semibold  text-white max-w-[867px] text-center">
+      <p className="leading-[134%] 2xl:text-[40px] sm:text-3xl text-[24px] font-semibold  text-white max-w-[867px] text-center">
         Accelerate your growth with a fully remote team of vetted designers.
         Unlimited design projects. Fast turnaround. All at a fixed monthly rate.
       </p>
-      <div className="inline-flex text-white font-semibold 2xl:text-[28px] text-2xl leading-[134%] tracking-[3px] gap-[47px] mt-20 mb-14">
+      <div className="inline-flex text-white font-semibold 2xl:text-[28px] text-2xl leading-[134%] tracking-[3px] sm:gap-[47px] gap-4 sm:mt-20 mt-14 flex-wrap flex-col items-center justify-center">
         <p>NO HR</p>
         <p>NO HIRING</p>
         <p>NO HOURLY BILLING</p>
--- src/components/why_grafixy/WhyGetGrafixy .jsx
+++ src/components/why_grafixy/WhyGetGrafixy .jsx
@@ -4,19 +4,19 @@ import { getGrafixy } from "../../lib/dummyData";
 
 const WhyGetGrafixy = () => {
   return (
-    <div className="2xl:px-[147px] sm:px-[80px] px-8 py-20 flex items-center justify-center flex-col ">
-      <div className="gap-1 grid items-center justify-center text-center">
-        <h1 className="font-semibold 2xl:text-[45px] text-4xl tracking-[-2%] text-[#210153]">
+    <div className="2xl:px-[147px] sm:px-[80px] px-8 2xl:py-20 lg:py-14 sm:py-10 py-8 flex items-center justify-center flex-col ">
+      <div className="gap-2 grid items-center justify-center text-center">
+        <h1 className="font-semibold 2xl:text-[45px] sm:text-4xl text-3xl tracking-[-2%] text-[#210153]">
           Why get grafixy?
         </h1>
-        <p className="text-[#6E3CBC] font-nunito 2xl:text-2xl text-xl max-w-[837px]">
+        <p className="text-[#6E3CBC] font-nunito 2xl:text-2xl lg:text-xl text-lg max-w-[837px] leading-[100%]">
           Maybe its time to stop hiring designers that are unreliable and
           expensive. Get your own design team at just a fraction of the cost.
         </p>
       </div>
-      <div className="grid grid-cols-2 2xl:mt-20 mt-16 2xl:gap-16 gap-14">
+      <div className="grid md:grid-cols-2 2xl:mt-20 lg:mt-16 mt-10 2xl:gap-16 lg:gap-14 gap-8 place-items-start md:w-max w-full">
         {getGrafixy.map((item) => (
-          <div key={item.id} className="flex items-center justify-start 2xl:gap-6 gap-5">
+          <div key={item.id} className="flex items-center justify-start 2xl:gap-6 lg:gap-5 gap-4">
             <div className="grid place-content-center w-24 h-24">
               <MainImage
                 src={item.icon}
@@ -27,8 +27,8 @@ const WhyGetGrafixy = () => {
               />
             </div>
             <div className="grid place-content-start gap-1">
-              <h3 className="font-medium 2xl:text-[28px] text-2xl tracking-[-2%] text-[#28075A] ">{item.title}</h3>
-              <p className="font-nunito 2xl:text-lg text-base font-normal text-[#6E3CBC] max-w-[310px]">{item.subtitle}</p>
+              <h3 className="font-medium 2xl:text-[28px] lg:text-2xl text-xl tracking-[-2%] text-[#28075A] ">{item.title}</h3>
+              <p className="font-nunito 2xl:text-lg lg:text-base text-sm leading-[100%] font-normal text-[#6E3CBC] max-w-[310px]">{item.subtitle}</p>
             </div>
           </div>
         ))}
