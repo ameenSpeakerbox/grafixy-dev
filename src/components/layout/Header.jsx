fbef6abc589ee99df9e92b7b7101177c07d4081b
MdodoIng <mdodoyt2@gmail.com>
2023-02-06 15:46:08 +0530

    modified:   src/components/layout/Header.jsx

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,42 +1,155 @@
-import React from "react";
-import { StaticImage } from "gatsby-plugin-image";
+import React, { useState } from "react";
 import { Button } from "../../ui/Button";
 import { Link } from "gatsby";
 import { navLink } from "../../lib/dummyData";
+import logo from "../../images/logo.png";
+import { ArrowWithLineIcon } from "../../ui/Icon";
 
 const Header = () => {
-  
+  const [isMenuClick, setIsMenuClick] = useState(false);
+
   return (
-    <nav className="relative h-[.1px] flex items-center  w-full ">
-      <div className="px-[147px] flex absolute z-50 h-[67px] items-center justify-between w-full left-0 top-0 mt-[90px]">
-        <Link to="/">
-        <StaticImage
-          src="../../images/logo.png"
-          alt="logo"
-          layout="fullWidth"
-          objectFit="contain"
-          className="w-[205px]"
-          />
+    <>
+      <nav
+        className="relative sm:h-[.1px] h-[110px] flex items-center  w-full bg-[#1E014B]"
+      >
+        <div className="2xl:px-[147px] sm:px-[80px] px-8  flex sm:absolute z-50 h-[67px] items-center justify-between w-full left-0 top-0 2xl:mt-[50px] sm:mt-6 overflow-hidden">
+          <Link to="/">
+            <img
+              src={logo}
+              alt="logo"
+              loading="eager"
+              layout="fullWidth"
+              className="sm:h-full h-[35px] object-contain"
+            />
           </Link>
-        <div className="flex items-center justify-center">
-          <div className="flex items-center justify-center gap-[50px] mr-[35px] ">
+          <div className="lg:flex hidden items-center justify-center w-full">
+            <div className=" lg:flex hidden items-center 2xl:gap-[50px] justify-end mr-[35px] lg:gap-10 gap-4 w-full">
+              {navLink.map((nav) => (
+                <Link
+                  key={nav.id}
+                  to={`/${nav.slug}`}
+                  className="text-white font-bold 2xl:text-lg xl:text-base text-[10px]"
+                >
+                  {nav.name}
+                </Link>
+              ))}
+            </div>
+            <Button
+              className="2xl:h-[67px] xl:h-[50px] h-[40px] bg-[#6019CE] mr-[10px] drop-shadow-lg text-white 2xl:px-[31px] 2xl:py-[10px] px-5 py-3 rounded-full shrink-0 2xl:text-lg xl:text-base text-[10px] grid place-content-center"
+              name="LOGIN"
+            />
+            <Button
+              className="2xl:h-[67px] xl:h-[50px] h-[40px] grid place-content-center  bg-white text-[#19013F] drop-shadow-lg 2xl:px-[31px] 2xl:py-[10px] px-5 py-3 rounded-full shrink-0 2xl:text-lg xl:text-base text-[10px]"
+              name="SCHEDULE A DEMO"
+            />
+          </div>
+
+          {/* mobile menu */}
+
+          <div
+            onClick={() => setIsMenuClick(!isMenuClick)}
+            role="button"
+            tabIndex={0}
+            className={`w-[40px] h-[40px] border-[3px] border-[#6019CE] rounded-md lg:hidden flex flex-col justify-evenly p-1 cursor-pointer relative overflow-hidden ${
+              isMenuClick && "items-center justify-center p-2"
+            }`}
+          >
+            <span
+              className={`h-[0px] border border-white right-1 left-1 bg-white duration-300 absolute transition-all ${
+                isMenuClick ? "rotate-45 " : "rotate-0 top-2"
+              }`}
+            />
+            <span
+              className={`h-[0px] border border-white duration-300 bg-white ${
+                isMenuClick ? "opacity-0" : "opacity-100"
+              }`}
+            />
+            <span
+              className={`h-[0px] border border-white  bg-white duration-300 absolute left-1 right-1 ${
+                isMenuClick ? "-rotate-45 " : "rotate-0 bottom-2"
+              }`}
+            />
+          </div>
+        </div>
+        {/* toggleMenu */}
+
+        <nav
+          className={`w-full  h-screen bg-[#1E014B] fixed top-0 z-50 ${
+            isMenuClick ? "block lg:hidden" : "hidden"
+          }`}
+        >
+          <div className="h-[110px] flex items-center  w-full bg-[#1E014B] border-b border-[#5A3D85] sm:px-[80px] px-8 justify-between">
+            <Link to="/">
+              <img
+                src={logo}
+                alt="logo"
+                loading="eager"
+                layout="fullWidth"
+                className="sm:h-full h-[35px] object-contain"
+              />
+            </Link>
+
+            <button
+              onClick={() => setIsMenuClick(!isMenuClick)}
+              role="button"
+              tabIndex={0}
+              className={`w-[40px] h-[40px] border-[3px] border-[#6019CE] rounded-md lg:hidden flex flex-col justify-evenly p-1 cursor-pointer relative ${
+                isMenuClick && "items-center justify-center p-2"
+              }`}
+            >
+              <span
+                className={`h-[0px] border border-white right-1 left-1 bg-white duration-300 absolute ${
+                  isMenuClick ? "rotate-45 " : "rotate-0 top-2"
+                }`}
+              />
+              <span
+                className={`h-[0px] border border-white  bg-white duration-300 absolute left-1 right-1 ${
+                  isMenuClick ? "-rotate-45 " : "rotate-0 bottom-2"
+                }`}
+              />
+            </button>
+          </div>
+
+          <div className=" flex flex-col justify-center items-center">
             {navLink.map((nav) => (
-              <Link key={nav.id} to={`/${nav.slug}`} className="text-white font-bold text-lg">
+              <Link
+                key={nav.id}
+                to={`/${nav.slug}`}
+                className="sm:h-[110px] h-[80px] flex items-center border-b border-[#5A3D85] text-xl font-semibold text-white px-8 sm:px-[80px] w-full"
+              >
                 {nav.name}
               </Link>
             ))}
+            <div className="sm:px-[80px] px-8 w-full grid sm:place-content-start ">
+              <Button
+                className="mt-10 w-full bg-white rounded-full text-sm text-[#6E3CBC] px-[22px] py-[9px] h-[50px]"
+                name="SCHEDULE A DEMO"
+              />
+            </div>
+
+            <button
+              onClick={() => setIsMenuClick(false)}
+              role="button"
+              tabIndex={0}
+              className="inline-flex sm:hidden items-center justify-center text-lg text-white gap-2 mt-5 "
+            >
+              Back to home
+            </button>
           </div>
-          <Button
-            className="h-[67px] bg-[#19013F] mr-[10px] drop-shadow-lg text-white px-[31px] py-[10px] rounded-full"
-            name="CLIENT LOGIN"
-          />
-          <Button
-            className="h-[67px] bg-white text-[#19013F] drop-shadow-lg px-[31px] py-[10px] rounded-full"
-            name="SCHEDULE A DEMO"
-          />
-        </div>
-      </div>
-    </nav>
+
+          <button
+            onClick={() => setIsMenuClick(false)}
+            role="button"
+            tabIndex={0}
+            className="sm:inline-flex hidden items-center justify-center text-lg text-white gap-2 absolute bottom-12 sm:right-[80px] right-8 "
+          >
+            <ArrowWithLineIcon />
+            back to home
+          </button>
+        </nav>
+      </nav>
+    </>
   );
 };
 
