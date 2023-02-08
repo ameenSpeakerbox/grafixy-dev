9d52599d4b54ef20b0c8b8d33cd29f471f43c457
MdodoIng <mdodoyt2@gmail.com>
2023-02-08 15:52:07 +0530

    modified:   gatsby-config.js 	modified:   package-lock.json 	modified:   package.json 	new file:   src/images/favicon.png

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,16 +7,29 @@ module.exports = {
     siteUrl: `https://www.yourdomain.tld`,
     description: `Example project for the Gatsby Head API`,
     twitterUsername: `@gatsbyjs`,
-    image: `/gatsby-icon.png`,
+    image: `src/images/favicon.png`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "React go",
+        short_name: "Reactgo",
+        start_url: "/",
+        background_color: "#f7f0eb",
+        theme_color: "#a2466c",
+        display: "standalone",
+        icon: "src/images/favicon.png",
+      },
+    },
     `gatsby-plugin-postcss`,
     `gatsby-plugin-sass`,
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     "gatsby-plugin-netlify",
-    "gatsby-plugin-typescript",`gatsby-plugin-offline`,
+    "gatsby-plugin-typescript",
+    `gatsby-plugin-offline`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -24,5 +37,6 @@ module.exports = {
         path: `${__dirname}/src/images/`,
       },
     },
+    
   ],
 };
