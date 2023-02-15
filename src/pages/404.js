afed50e4561da65e2d5ebb0289644f62abb8021b
ameenSpeakerbox <ameen466speakerbox@gmail.com>
2023-02-15 14:26:04 +0530

    new file:   src/images/not fond.svg 	deleted:    src/images/not fond.webp 	modified:   src/pages/404.js 	modified:   src/style/global.css

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,24 +1,26 @@
 import * as React from "react";
 import { Link } from "gatsby";
 import Header from "../components/layout/Header";
-import bg from "../images/not fond.webp";
+import bg from "../images/not fond.svg";
 import { MainImage } from "gatsby-plugin-image";
 
 const NotFoundPage = () => {
   return (
-    <main className="relative h-screen">
-      <MainImage
+    <main className="relative h-screen overflow-hidden notFoundPageBgGradient z-0" >
+     
+      <Header />
+      <div className="2xl:px-[147px] sm:px-[80px] px-8 lg:pt-40 sm:pt-32 pt-20 flex items-start justify-center h-full ">
+        <h1 className="font-medium text-4xl text-[#9C66F0] text-center">
+          This page is <span className="text-white">under construction</span>
+        </h1>
+
+        <MainImage
         alt="bg"
         src={bg}
         width={100}
         height={100}
-        className="absolute w-full object-center object-contain top-0 left-0 -z-10"
+        className="absolute max-w-[600px] object-center object-contain w-full h-auto lg:bottom-10 sm:bottom-20 bottom-40 -z-10"
       />
-      <Header />
-      <div className="2xl:px-[147px] sm:px-[80px] px-8 lg:pt-40 sm:pt-32 pt-20 flex items-center justify-center">
-        <h1 className="font-medium text-4xl text-[#9C66F0]">
-          This page is <span className="text-white">under construction</span>
-        </h1>
       </div>
     </main>
   );
