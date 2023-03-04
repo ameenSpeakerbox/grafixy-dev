cf571a230499d055422bc194322bbf3ff8cd96ec
ameenSpeakerbox <ameen466speakerbox@gmail.com>
2023-03-04 13:42:29 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/components/layout/layout.jsx

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -3,13 +3,20 @@ import TawkMessengerReact from "@tawk.to/tawk-messenger-react";
 import Footer from "./Footer";
 import Header from "./Header";
 import { Script } from "gatsby";
+import icon from '../../images/favicon.svg'
+import { FloatingWhatsApp } from "react-floating-whatsapp";
 
 const Layout = ({ children }) => {
   return (
     <React.Fragment>
-      <TawkMessengerReact
+      {/* <TawkMessengerReact
         propertyId="63bfb5b447425128790d012d"
         widgetId="1gmidriej"
+      /> */}
+      <FloatingWhatsApp
+        phoneNumber="00 000 00 00"
+        accountName={"Grafixy"}
+        avatar={icon}
       />
 
       <Header />
@@ -23,6 +30,6 @@ export const head = () => (
   <Script
     src="https://js.chargebee.com/v2/chargebee.js"
     data-cb-site="speakerbox"
-   />
+  />
 );
 export default Layout;
